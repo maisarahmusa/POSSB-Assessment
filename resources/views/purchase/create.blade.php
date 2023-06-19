3ff0f7f07a4180bad1ea5e77eae18d44307e3dc2
Maisarah <maisarahmusa1998@gmail.com>
2023-06-19 18:19:27 +0800

    view

--- resources/views/purchase/create.blade.php
+++ resources/views/purchase/create.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.landing-base')
-
-@section('content')
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/css/bootstrap-select.min.css" integrity="sha512-mR/b5Y7FRsKqrYZou7uysnOdCIJib/7r5QeJMFvLNHNhtye3xJp1TdJVPLtetkukFn227nKpXD9OjUc09lx97Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+@section('content')
 
 <div class="card mt-6">
     <div class="card-body">
